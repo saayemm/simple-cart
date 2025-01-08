28da913e220829e565f0b18a657a628a86d40517
Saayemm <sayemdcc@gmail.com>
2025-01-08 09:46:09 +0600

    ls

---
+++ src/utilities/localstroage.js
@@ -0,0 +1,20 @@
+const getStoredCart = () => {
+   const storedCartString =  localStorage.getItem("cart")
+   if(storedCartString){
+    return JSON.parse(storedCartString)
+   }
+   return []
+}
+
+const saveCartToLs = cart => {
+    const cartStringify = JSON.stringify(cart)
+    localStorage.setItem("cart", cartStringify)
+}
+
+const AddToLs = id => {
+    const cart = getStoredCart();
+    cart.push(id)
+    saveCartToLs(cart)
+}
+
+export {AddToLs}
